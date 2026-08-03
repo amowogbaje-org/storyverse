d10b5d33765ec39a0d7926cb4eb19233877041a3
Gideon Amowogbaje <amowogbajegideon@gmail.com>
2026-08-03 09:31:05 +0100

    there you go

--- backend/database/migrations/2026_07_26_142648_create_agent_conversations_table.php
+++ backend/database/migrations/2026_07_26_142648_create_agent_conversations_table.php
@@ -15,6 +15,7 @@ return new class extends AiMigration
         $messagesTable = config('ai.conversations.tables.messages', 'agent_conversation_messages');
 
         Schema::create($conversationsTable, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->string('id', 36)->primary();
             $table->string('participant_type')->nullable();
             $table->unsignedBigInteger('participant_id')->nullable();
@@ -25,6 +26,7 @@ return new class extends AiMigration
         });
 
         Schema::create($messagesTable, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->string('id', 36)->primary();
             $table->string('conversation_id', 36)->index();
             $table->string('participant_type')->nullable();
